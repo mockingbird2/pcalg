9cdf1ac587b59126d126a46cb721993c274d524b
Fabian Paul <fabian.paul@student.hpi.de>
2018-06-14 14:53:31 +0200

    Use process parallelization instead of threads

--- parallelskeleton.py
+++ parallelskeleton.py
@@ -1,6 +1,4 @@
-from __future__ import print_function
-
-from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing import Pool
 
 from itertools import combinations, permutations, chain
 import logging
@@ -10,44 +8,51 @@ from pcalg import _create_complete_graph
 _logger = logging.getLogger(__name__)
 
 
-def splitted_task(level, indep_test_func, data_matrix, kwargs, stable, alpha,
-                  graph):
-    node_size = data_matrix.shape[1]
+class Task:
+    def __init__(self, level, indep_test_func, data_matrix, kwargs, stable,
+                 alpha, graph):
+        self.level = level
+        self.indep_test_func = indep_test_func
+        self.data_matrix = data_matrix
+        self.node_size = data_matrix.shape[1]
+        self.kwargs = kwargs
+        self.stable = stable
+        self.alpha = alpha
+        self.graph = graph
 
-    def task(edges):
-        sep_set = [[set() for i in range(node_size)] for j in range(node_size)]
+    def run(self, edges):
+        sep_set = [[set() for i in range(self.node_size)] for j in
+                   range(self.node_size)]
         remove_edges = []
         i, j = edges
-        adj_i = list(graph.neighbors(i))
+        adj_i = list(self.graph.neighbors(i))
         if j not in adj_i:
             return True, sep_set, remove_edges
         else:
             adj_i.remove(j)
-        if len(adj_i) >= level:
+        if len(adj_i) >= self.level:
             _logger.debug('testing %s and %s' % (i, j))
             _logger.debug('neighbors of %s are %s' % (i, str(adj_i)))
-            if len(adj_i) < level:
+            if len(adj_i) < self.level:
                 return True, sep_set, remove_edges
-            for k in combinations(adj_i, level):
+            for k in combinations(adj_i, self.level):
                 _logger.debug('indep prob of %s and %s with subset %s'
                               % (i, j, str(k)))
-                p_val = indep_test_func(data_matrix, i, j, set(k),
-                                        **kwargs)
+                p_val = self.indep_test_func(self.data_matrix, i, j, set(k),
+                                             **self.kwargs)
                 _logger.debug('p_val is %s' % str(p_val))
-                if p_val > alpha:
-                    if graph.has_edge(i, j):
+                if p_val > self.alpha:
+                    if self.graph.has_edge(i, j):
                         _logger.debug('p: remove edge (%s, %s)' % (i, j))
-                        if stable:
+                        if self.stable:
                             remove_edges.append((i, j))
                         else:
-                            graph.remove_edge(i, j)
+                            self.graph.remove_edge(i, j)
                     sep_set[i][j] |= set(k)
                     sep_set[j][i] |= set(k)
                     break
             return True, sep_set, remove_edges
         return False, sep_set, remove_edges
-
-    return task
 
 
 def merge_sep_sets(sep_sets):
@@ -74,10 +79,10 @@ def estimate_skeleton_parallel(indep_test_func, data_matrix, alpha, **kwargs):
     cont = True
     sep_sets = []
     while cont:
-        task = splitted_task(level, indep_test_func, data_matrix, kwargs,
-                             stable, alpha, g)
-        with Pool(2) as p:
-            results = p.map(task, edges_permutations)
+        task = Task(level, indep_test_func, data_matrix, kwargs,
+                    stable, alpha, g)
+        with Pool(10) as p:
+            results = p.map(task.run, edges_permutations)
         conts, next_sep_sets, removable_edges = zip(*results)
         sep_sets.append(next_sep_sets)
         remove_edges = filter(None, removable_edges)

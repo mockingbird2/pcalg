70eda6b8f9e5041f34bae88a9bc1bb73bcbd4ecd
Antje Schubotz <antje.schubotz@gmail.com>
2018-06-20 16:49:46 +0200

    handle separation sets more efficiently

--- skeletonmethods/parallelskeleton.py
+++ skeletonmethods/parallelskeleton.py
@@ -22,13 +22,12 @@ class Task:
         self.graph = graph
 
     def run(self, edges):
-        sep_set = [[set() for i in range(self.node_size)] for j in
-                   range(self.node_size)]
+        sep_set = []
         remove_edges = []
         i, j = edges
         adj_i = list(self.graph.neighbors(i))
         if j not in adj_i:
-            return False, [], remove_edges
+            return False, sep_set, remove_edges
         else:
             adj_i.remove(j)
         if len(adj_i) >= self.level:
@@ -49,21 +48,24 @@ class Task:
                             remove_edges.append((i, j))
                         else:
                             self.graph.remove_edge(i, j)
-                    sep_set[i][j] |= set(k)
-                    sep_set[j][i] |= set(k)
+                    sep_set.append((i, j, set(k)))
+                    sep_set.append((j, i, set(k)))
                     break
             return True, sep_set, remove_edges
-        return False, [], remove_edges
+        return False, sep_set, remove_edges
 
 
-def merge_sep_sets(sep_sets):
-    sep_set = next(sep_sets)
-    for sep in sep_sets:
-        for i in range(len(sep)):
-            for j in range(len(sep[i])):
-                sep_set[i][j] |= sep[i][j]
-                sep_set[j][i] |= sep[j][i]
-    return sep_set
+def merge_sep_sets(size, sep_sets):
+    result = [[set() for i in range(size)] for j in range(size)]
+    for sep_set in sep_sets:
+        if sep_set == []:
+            continue
+        for x in sep_set:
+            i = x[0]
+            j = x[1]
+            k = x[2]
+            result[i][j] |= k
+    return result
 
 
 def estimate_skeleton_parallel(indep_test_func, data_matrix, alpha, **kwargs):
@@ -85,7 +87,7 @@ def estimate_skeleton_parallel(indep_test_func, data_matrix, alpha, **kwargs):
         with Pool(10) as p:
             results = p.map(task.run, edges_permutations)
         conts, next_sep_sets, removable_edges = zip(*results)
-        sep_sets.append(next_sep_sets)
+        sep_sets.extend(next_sep_sets)
         remove_edges = filter(None, removable_edges)
         cont = any(conts)
         level += 1
@@ -93,14 +95,14 @@ def estimate_skeleton_parallel(indep_test_func, data_matrix, alpha, **kwargs):
             g.remove_edges_from(chain(*remove_edges))
         if ('max_reach' in kwargs) and (level > kwargs['max_reach']):
             break
-    sep_set = merge_sep_sets(chain(*sep_sets))
+    sep_set = merge_sep_sets(data_matrix.shape[1], sep_sets)
     return g, sep_set
 
 def estimate_skeleton_naive(indep_test_func, data_matrix, alpha, **kwargs):
     node_ids = range(data_matrix.shape[1])
     complete_graph = _create_complete_graph(node_ids)
     g, sep_sets = estimate_skeleton_naive_step(indep_test_func, data_matrix, alpha, 0, complete_graph, **kwargs)
-    sep_set = merge_sep_sets(chain(*sep_sets))
+    sep_set = merge_sep_sets(data_matrix.shape[1], sep_sets)
     return g, sep_set
 
 def estimate_skeleton_naive_step(indep_test_func, data_matrix, alpha, level, g, **kwargs):
@@ -123,7 +125,7 @@ def estimate_skeleton_naive_step(indep_test_func, data_matrix, alpha, level, g, 
         with Pool(10) as p:
             results = p.map(task.run, edges_permutations)
         conts, next_sep_sets, removable_edges = zip(*results)
-        sep_sets.append(next_sep_sets)
+        sep_sets.extend(next_sep_sets)
         remove_edges = filter(None, removable_edges)
         cont = any(conts)
         level += 1
